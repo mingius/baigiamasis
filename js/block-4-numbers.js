4043b1de2ac0619750a7729f268cefe90a499eb5
AudriusM <audriusmingela@gmail.com>
2017-06-01 15:56:46 +0300

    paskutiniai

--- js/block-4-numbers.js
+++ js/block-4-numbers.js
@@ -1,41 +1,42 @@
 /**
  * Created by namai on 5/26/2017.
- // */
- // $('.count').each(function () {
- //     $(this).prop('Counter',0).animate({
- //         Counter: $(this).text()
- //     }, {
- //         duration: 4000,
- //         easing: 'swing',
- //         step: function (now) {
- //            $(this).text(Math.ceil(now));
- //         }
- //     });
- // });
-
-$(function() {
-    var oTop = $('.count').offset().top - window.innerHeight;
-    $(window).scroll(function(){
-
-        var pTop = $('body').scrollTop();
-        console.log( pTop + ' - ' + oTop );
-        if( pTop > oTop ){
-            start_count();
+ */
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
         }
     });
 });
 
-function start_count(){
+// $(function() {
+//     var oTop = $('.numbers').offset().top - window.innerHeight;
+//     $(window).scroll(function(){
+//
+//         var pTop = $('body').scrollTop();
+//         console.log( pTop + ' - ' + oTop );
+//         if( pTop > oTop ){
+//             start_count();
+//         }
+//     });
+// });
+//
+// function start_count(){
+//
+//     $('.count').each(function () {
+//         $(this).prop('.Counter',0).animate({
+//             Counter: $(this).text()
+//         }, {
+//             duration: 4000,
+//             easing: 'swing',
+//             step: function (now) {
+//                 $(this).text(Math.ceil(now));
+//             }
+//         });
+//     });
+// }
 
-    $('.count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 4000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
-}
--- js/main.js
+++ js/main.js
@@ -10,4 +10,3 @@ $(document).ready(function(){
         $(this).tab('show');
     });
 });
-
